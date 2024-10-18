01d2fb88b0a9dfe1796f1b91701f8a2e69405aa1
depavlovic <pavlovicdejan003@gmail.com>
2024-10-18 18:05:23 +0200

    change search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,42 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Movie } from '../movie/movie.model';
 import { MovieListService } from '../movie-list.service';
+import { FormsModule } from '@angular/forms'
+
+
+
+
 
 
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [MatFormFieldModule,MatCardModule,MatButtonModule,MatInputModule,MatSelectModule,NgFor],
+  imports: [MatFormFieldModule,MatCardModule,MatButtonModule,MatInputModule,MatSelectModule,NgFor,FormsModule,NgIf],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent implements OnInit {
   movies: Movie[] = [];
   filteredMovies: Movie[]=[];
+  searchResults: Movie[] = []
+
+  searchCriteria = {
+    title: '',
+    genre: '',
+    actors: '',
+    duration: '',
+    ticketPrice: ''
+  };
   
-  searchTerm: string = '';
+  
 
   constructor(private movieListService: MovieListService) {} 
+
+
 
   ngOnInit(): void {
     this.movies = this.movieListService.getMovies(); 
     this.filteredMovies = this.movies;
   }
 
-  onSearch() {
-    const term = this.searchTerm.toLowerCase();
-    this.filteredMovies = this.movies.filter(movie =>
-      movie.title.toLowerCase().includes(term) ||
-      movie.genre.toLowerCase().includes(term) ||
-      movie.cast.some(actor => actor.toLowerCase().includes(term))
-    );
-  }
+  doSearch() {
+    const { title, genre, actors, duration, ticketPrice } = this.searchCriteria;
+
+    this.searchResults = this.filteredMovies.filter(movie => {
+
+      return (!title || movie.title.toLowerCase().includes(title.toLowerCase())) &&
+             (!genre || movie.genre.toLowerCase() === genre.toLowerCase()) &&
+             (!actors || movie.cast.join(',').toLowerCase().includes(actors.toLowerCase())) &&
+             (!duration || movie.duration === duration) &&
+             (!ticketPrice || movie.ticketPrice.toString() === ticketPrice);
+    });
+    console.log('Search results:', this.searchResults);
+
+}
 
 }
